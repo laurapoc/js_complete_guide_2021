7255385f68a60f014237d7392ca2ce031e0dcf80
laurapoc <laura.pociuniene@gmail.com>
2021-09-13 11:28:04 +0300

    add user choises to the game

--- functions-01-starting-code/functions-01-starting-code/app.js
+++ functions-01-starting-code/functions-01-starting-code/app.js
@@ -1,17 +1,27 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-function startGame() {
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOISE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoise = function () {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, "").toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choise! We chose ${DEFAULT_USER_CHOISE} for you!`);
+    return DEFAULT_USER_CHOISE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log("game is starting here");
-}
-
-// You can have functions in objects and call them methods:
-// const person = {
-//   name: "Max",
-//   greet: function greet() {
-//     console.log("Hello there!");
-//   },
-// };
-
-// person.greet();
-
-startGameBtn.addEventListener("click", startGame);
+  const playerSelection = getPlayerChoise();
+  console.log(playerSelection);
+});
