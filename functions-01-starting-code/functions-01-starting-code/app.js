4106e3f57e6f6b9b50d5743a762ae832b2c9f481
laurapoc <laura.pociuniene@gmail.com>
2021-09-13 10:18:15 +0300

    add function in object example

--- functions-01-starting-code/functions-01-starting-code/app.js
+++ functions-01-starting-code/functions-01-starting-code/app.js
@@ -1,1 +1,17 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("game is starting here");
+}
+
+// You can have functions in objects and call them methods:
+// const person = {
+//   name: "Max",
+//   greet: function greet() {
+//     console.log("Hello there!");
+//   },
+// };
+
+// person.greet();
+
+startGameBtn.addEventListener("click", startGame);
