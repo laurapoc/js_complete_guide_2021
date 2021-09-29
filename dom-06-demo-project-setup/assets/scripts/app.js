481441780a36b57dfb189de1a24a0256d44e1a08
Laura Pociuniene <laurapociuniene@gmail.com>
2021-09-29 14:18:45 +0300

    add basic input validation

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -5,6 +5,8 @@ const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
 
+const movies = [];
+
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
 };
@@ -13,9 +15,15 @@ const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
   toggleBackdrop();
 };
+const clearMovieInput = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = "";
+  }
+};
 
 const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
 };
 
 const addMovieHandler = () => {
@@ -32,6 +40,17 @@ const addMovieHandler = () => {
     alert("Please enter valid values (rating between 1 and 5.)");
     return;
   }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
 };
 
 const backdropClickHandler = () => {
